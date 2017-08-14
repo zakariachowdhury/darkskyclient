402af5aae34d231af868fdfa6e87596eb37b8ed7
Zakaria Chowdhury <zakaria.cse@gmail.com>
2017-08-14 08:47:39 -0400

    Get device location using Google Maps API

--- app/src/main/java/com/zakariachowdhury/darkskyclient/MainActivity.java
+++ app/src/main/java/com/zakariachowdhury/darkskyclient/MainActivity.java
@@ -1,6 +1,13 @@
 package com.zakariachowdhury.darkskyclient;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
@@ -8,6 +15,9 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.zakariachowdhury.darkskyclient.event.ErrorEvent;
 import com.zakariachowdhury.darkskyclient.event.WeatherEvent;
 import com.zakariachowdhury.darkskyclient.model.Currently;
@@ -21,8 +31,9 @@ import org.greenrobot.eventbus.ThreadMode;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
 
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     @BindView(R.id.temperatureContainer)
     LinearLayout temperatureContainer;
 
@@ -35,18 +46,22 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.iconImageView)
     ImageView iconImageView;
 
+    private GoogleApiClient mGoogleApiClient;
+    private boolean mLocationPermissionGranted = false;
+    private Location mLastKnownLocation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        initGoogleApiClient();
     }
 
     @Override
     public void onStart() {
         super.onStart();
         EventBus.getDefault().register(this);
-        getLocation();
     }
 
     @Override
@@ -73,8 +88,76 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, errorEvent.getMessage(), Toast.LENGTH_LONG).show();
     }
 
-    private void getLocation() {
-        WeatherApi weatherApi = new WeatherApi(this);
-        weatherApi.getWeather(42.6340327, -83.2184753);
+    private void requestCurrentWeather() {
+        getDeviceLocation();
+
+        if (mLocationPermissionGranted && mLastKnownLocation != null) {
+            double lat = mLastKnownLocation.getLatitude();
+            double lng = mLastKnownLocation.getLongitude();
+
+            WeatherApi weatherApi = new WeatherApi(this);
+            weatherApi.getWeather(lat, lng);
+        } else {
+            Toast.makeText(this, R.string.error_google_map_api, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void initGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */,
+                        this /* OnConnectionFailedListener */)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    private void getDeviceLocation() {
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+
+        if (mLocationPermissionGranted) {
+            mLastKnownLocation = LocationServices.FusedLocationApi
+                    .getLastLocation(mGoogleApiClient);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String permissions[],
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mLocationPermissionGranted = true;
+                }
+            }
+        }
+        requestCurrentWeather();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this, R.string.error_google_map_api, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        requestCurrentWeather();
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Toast.makeText(this, R.string.error_google_map_api, Toast.LENGTH_LONG).show();
     }
 }
